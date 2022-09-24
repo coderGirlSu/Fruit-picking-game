e9bb38cddc231732e8d79d6a8ca8257871d464f7
Su Zhang <suzhang79@gmail.com>
2022-09-24 19:31:46 +1000

    adjust position

--- fruit-game/src/App.js
+++ fruit-game/src/App.js
@@ -19,15 +19,15 @@ function App() {
       <img id="tree" src="images/tree.png" alt="tree" />
 
       <Fruit id="fruit1" />
-      <Fruit />
-      <Fruit />
-      <Fruit />
-      <Fruit />
-      <Fruit />
-      <Fruit />
-      <Fruit />
-      <Fruit />
-      <Fruit />
+      <Fruit id="fruit2" />
+      <Fruit id="fruit3" />
+      <Fruit id="fruit4" />
+      <Fruit id="fruit5" />
+      <Fruit id="fruit6" />
+      <Fruit id="fruit7" />
+      <Fruit id="fruit8" />
+      <Fruit id="fruit9" />
+      <Fruit id="fruit10" />
       <Score store={store}/>
     </div>
   );
