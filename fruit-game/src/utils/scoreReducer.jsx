02b8e5dc560a21493ca5dbcd883055c8aa6022a8
Su Zhang <suzhang79@gmail.com>
2022-09-27 23:18:32 +1000

    fix spider score

--- fruit-game/src/utils/scoreReducer.jsx
+++ fruit-game/src/utils/scoreReducer.jsx
@@ -2,7 +2,7 @@ export default function reducer(state,action){
     switch(action.type){
         case 'updateScore':{
             let newScore
-            if (action.data === '🐛'){
+            if (action.data === '🐛' || action.data === '🕷' ){
                 newScore = state.score - 50
             }else{
                 newScore = state.score + 10
